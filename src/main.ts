552252b13d270438a9624bde68e1d375002e4cc8
Kim Gustyr <kim.gustyr@flagsmith.com>
2026-08-22 21:47:46 +0100

    Default repository input to the current GitHub repository

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { publish } from 'goreleaser-npm-publisher';
 import { cwd } from 'node:process';
 import { boolean, string, stringArray } from './inputs';
 import { logger } from './logger';
+import { defaultRepository } from './repository';
 
 export async function run(): Promise<void> {
   logger.debug(`Running publishing...`);
@@ -13,7 +14,7 @@ export async function run(): Promise<void> {
     name: string('name'),
     bin: string('bin'),
     prefix: string('prefix'),
-    repository: string('repository'),
+    repository: string('repository', defaultRepository()),
     repositoryType: string('repository-type'),
     repositoryDirectory: string('repository-directory'),
     description: string('description'),
